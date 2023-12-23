987b695bd3d3f73c62fdd530a1fd39b47c566f9b
Alan-X-Hacker <104323775+Alan-X-Hacker@users.noreply.github.com>
2023-12-23 16:12:19 -0600

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
 
+client.on('qr', (qr) => {
+  console.log('Escanear este código QR con WhatsApp:');
+  console.log(qr);
+});
+
+client.on('ready', () => {
+  console.log('¡El bot está listo para responder mensajes!');
+});
+
+client.on('message', async (message) => {
+  if (message.body === 'Hola') {
+    await message.reply('¡Hola! Soy un bot de WhatsApp.');
+  }
+});
+
+client.initialize();
